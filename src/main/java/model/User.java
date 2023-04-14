769ba67e50c4f48226ed493b5585342a1069c334
mobin <mobinpower007@gmail.com>
2023-04-14 17:52:44 +0430

    make User comparable and add some getters

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,21 +1,38 @@
 package model;
 
-public class User {
-    String username;
-    String password;
-    int highscore;
-    String slogan;
-    String email;
-    String securityQ;
-    String securityA;
+public class User implements Comparable<User>{
+    private String username;
+    private String password;
+    private int highScore;
+    private String slogan;
+    private String email;
+    private String securityQ;
+    private String securityA;
 
     public User(String username, String password, int highscore, String slogan, String email, String securityQ, String securityA) {
         this.username = username;
         this.password = password;
-        this.highscore = highscore;
+        this.highScore = highscore;
         this.slogan = slogan;
         this.email = email;
         this.securityQ = securityQ;
         this.securityA = securityA;
+    }
+
+    public int getHighScore() {
+        return highScore;
+    }
+
+    public String getSlogan() {
+        return slogan;
+    }
+
+    public boolean isPasswordCorrect(String password) {
+        return this.password.equals(password);
+    }
+
+    @Override
+    public int compareTo(User o) {
+        return o.highScore - this.highScore;
     }
 }
