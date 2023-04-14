5eb133f4b03dcc92d9669b7d4632ddfbc7954608
Mazdak <mazdak.tey@gmail.com>
2023-04-14 17:03:09 +0430

    added fields and methods to Man and Destructable

--- src/main/java/model/man/RangedSoldier.java
+++ src/main/java/model/man/RangedSoldier.java
@@ -1,4 +1,18 @@
 package model.man;
 
 public class RangedSoldier extends Soldier{
+    private int range;
+
+    public int getRange() {
+        return range;
+    }
+
+    public void setRange(int range) {
+        this.range = range;
+    }
+
+    public void isOnTopOfTower()
+    {
+
+    }
 }
