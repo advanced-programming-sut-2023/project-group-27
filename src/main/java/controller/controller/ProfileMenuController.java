f554c9955ab4b5c40c0648baa2fc0ad121062464
mobin <mobinpower007@gmail.com>
2023-04-14 17:51:30 +0430

    add ProfileMenuController with method signature

---
+++ src/main/java/controller/controller/ProfileMenuController.java
@@ -0,0 +1,47 @@
+package controller.controller;
+
+import model.User;
+
+public class ProfileMenuController {
+    User loggedInUser;
+
+    public String changeUsername(String username) {
+        return null;
+    }
+
+    public String changeNickname(String nickname) {
+        return null;
+    }
+
+    public boolean isPasswordValid(String oldPassword) {
+        return loggedInUser.isPasswordCorrect(oldPassword);
+    }
+
+    public String changePassword(String newPassword, String newPasswordConfirmation) {
+        return null;
+    }
+
+    public String changeSlogan(String newSlogan) {
+        return null;
+    }
+
+    public void removeSlogan() {
+
+    }
+
+    public int showHighScore() {
+        return loggedInUser.getHighScore();
+    }
+
+    public int showRank() {
+        return 1;
+    }
+
+    public String showSlogan() {
+        return loggedInUser.getSlogan();
+    }
+
+    public String showProfile() {
+        return null;
+    }
+}
