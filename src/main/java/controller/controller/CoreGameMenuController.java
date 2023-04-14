061390de94dd2f79fdc4b316e487d0e0c3a61026
Mazdak <mazdak.tey@gmail.com>
2023-04-14 21:33:23 +0430

    added trade and game controller features

--- src/main/java/controller/controller/CoreGameMenuController.java
+++ src/main/java/controller/controller/CoreGameMenuController.java
@@ -1,4 +1,63 @@
 package controller.controller;
 
+import model.User;
+import view.GameMenu;
+
 public class CoreGameMenuController {
+    private User loggedInUser;
+    private GameMenu gameMenu;
+
+    public CoreGameMenuController(User loggedInUser, GameMenu gameMenu) {
+        this.loggedInUser = loggedInUser;
+        this.gameMenu = gameMenu;
+    }
+
+    public String showPopularityFactors(){
+        return null;
+    }
+
+    public int showPopularity(){
+        return 0;
+    }
+
+    public String showFoodList(){
+        return null;
+    }
+
+    public String setFoodRate(int rate){
+        return null;
+    }
+
+    public String showFoodRate(){
+        return null;
+    }
+
+    public String setTaxRate(int rate){
+        return null;
+    }
+
+    public String showTaxRate(){
+        return null;
+    }
+
+    public String setFearRate(int rate){
+        return null;
+    }
+
+    public String showFearRate(){
+        return null;
+    }
+
+    public String dropBuilding(int x , int y , String type){
+        return null;
+    }
+
+    public String selectBuilding(int x , int y){
+        return null;
+    }
+
+    public String selectUnit(int x , int y){
+        return null;
+    }
+
 }
--- src/main/java/controller/controller/CoreTradeMenuController.java
+++ src/main/java/controller/controller/CoreTradeMenuController.java
@@ -1,4 +1,32 @@
 package controller.controller;
 
+import model.Goods;
+import model.User;
+import view.TradeMenu;
+
 public class CoreTradeMenuController {
+    private User loggedInUser;
+
+    private TradeMenu tradeMenu;
+
+    public CoreTradeMenuController(User loggedInUser, TradeMenu tradeMenu) {
+        this.loggedInUser = loggedInUser;
+        this.tradeMenu = tradeMenu;
+    }
+
+    public String showTradeList(){
+        return null;
+    }
+
+    public String showTradeHistory(){
+        return null;
+    }
+
+    public String acceptTrade(int id , String massage){
+        return null;
+    }
+
+    public String tradeRequest(Goods goods , int goodsAmount , int price , String massage){
+        return null;
+    }
 }
