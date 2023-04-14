f6a34edf8d9d65e242b9344aef87c037048e70ff
mobin <mobinpower007@gmail.com>
2023-04-14 16:40:43 +0430

    add enum Production and field and methods in ProductionBuilding

--- src/main/java/model/building/ProductionBuilding.java
+++ src/main/java/model/building/ProductionBuilding.java
@@ -1,4 +1,22 @@
 package model.building;
 
+import model.Production;
+
+import java.util.List;
+
 public class ProductionBuilding extends Building{
+    List<Production> requirements;
+    Production product;
+
+    public ProductionBuilding(List<Production> requirements, Production product) {
+        this.requirements = requirements;
+        this.product = product;
+    }
+
+    public void act() {
+        for (Production requirement : requirements) {
+            requirement.utilize();
+        }
+        product.produce();
+    }
 }
