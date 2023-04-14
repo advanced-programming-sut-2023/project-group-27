c58980eb30728e2aa444dc4f2602e06b1eafc626
Mazdak <mazdak.tey@gmail.com>
2023-04-14 22:02:12 +0430

    added main controller features

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,4 +1,19 @@
 package controller;
 
+import java.util.Scanner;
+
 public class Controller {
+    public void run(Scanner scanner){
+
+    }
+
+    public void openFile(){
+    }
+
+    public void saveFile(){
+    }
+
+    public void nextTurn(){
+
+    }
 }
