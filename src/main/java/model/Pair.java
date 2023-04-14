9eff30b0a4c1697a307e8eaf63b2d62b605debac
mobin <mobinpower007@gmail.com>
2023-04-14 20:44:19 +0430

    constructor and fields for task implementations

---
+++ src/main/java/model/Pair.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Pair<T, T1> {
+    public T x;
+    public T1 y;
+
+    public Pair(T first, T1 second) {
+        this.x = first;
+        this.y = second;
+    }
+}
--- src/main/java/model/task/Fight.java
+++ src/main/java/model/task/Fight.java
@@ -1,8 +1,16 @@
 package model.task;
 
+import model.Destructable;
+
 public class Fight implements Task {
+    private Destructable target;
+
     @Override
     public void run() {
 
+    }
+
+    public Fight(Destructable target) {
+        this.target = target;
     }
 }
