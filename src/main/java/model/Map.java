568120f4862fbab5ba2787703ad4fbdd404213fe
Mazdak <mazdak.tey@gmail.com>
2023-04-14 16:24:34 +0430

    added fields and methods to Map

--- src/main/java/model/Map.java
+++ src/main/java/model/Map.java
@@ -1,4 +1,23 @@
 package model;
 
 public class Map {
+    private final int size;
+    private final Cell [][] map;
+    public Map(int size) {
+        this.size = size;
+        map = new Cell[size][size];
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Cell[][] getMap() {
+        return map;
+    }
+
+    public Cell[][] showMap(int x , int y , int sizeToBeShown)
+    {
+        return null;
+    }
 }
