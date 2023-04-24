990255234375839188eb3aba1be37dabc4b5d1f1
Arshia <arshiayousefnia@yahoo.com>
2023-04-24 19:12:15 +0330

    added main menu

--- src/main/java/view/MainMenu.java
+++ src/main/java/view/MainMenu.java
@@ -1,6 +1,8 @@
 package view;
 
 import controller.view_controllers.MainMenuController;
+import model.StrongholdCrusader;
+import view.enums.MainMenuRegexes;
 
 import java.util.Scanner;
 
@@ -11,7 +13,27 @@ public class MainMenu {
         this.controller = controller;
     }
 
-    public String run(Scanner scanner){
-        return null;
+    public String run(Scanner scanner) {
+        do {
+            String command = scanner.nextLine();
+            if (MainMenuRegexes.LOGOUT.getMatcher(command).matches()) {
+                System.out.println("User " + StrongholdCrusader.getCurrentUser().getUsername() + " logged out.");
+                return "Logout";
+            }
+            if (MainMenuRegexes.ENTERPROFILEMENU.getMatcher(command).matches()) {
+                System.out.println("Entered Profile Menu");
+                return "Enter profile menu";
+            }
+            if (MainMenuRegexes.ENTERMAPEDITMENU.getMatcher(command).matches()) {
+                System.out.println("Entered Map Edit Menu");
+                return "Enter map edit menu";
+            }
+            if (MainMenuRegexes.ENTERGAMEMENU.getMatcher(command).matches()) {
+                System.out.println("Entered Game Menu");
+                return "Enter game menu";
+            }
+
+            System.out.println("Invalid command!");
+        } while (true);
     }
 }
