0d306ed3af475a48471fdad750bb45a373827cfd
Mobin-Pourabedini <realmobinpourabedini@gmail.com>
2023-04-28 13:39:43 +0430

    add MenuBase for common methods and fields in menus

---
+++ src/main/java/view/MenuBase.java
@@ -0,0 +1,31 @@
+package view;
+
+import view.enums.RegisterMenuRegexes;
+
+import java.util.Scanner;
+
+public class MenuBase {
+    private final Scanner scanner;
+
+    public MenuBase(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void showInformation(String info) {
+        System.out.println(info);
+    }
+
+    public boolean confirm(String message, String expectedValue) {
+        String confirmation = "";
+        System.out.println(message);
+        return this.scanner.nextLine().equals(expectedValue);
+    }
+
+    public String fetchAnswer() {
+        String answer = this.scanner.nextLine();
+        if (!RegisterMenuRegexes.SECURITY_A.getMatcher(answer).matches()) {
+            return null;
+        }
+        return answer;
+    }
+}
