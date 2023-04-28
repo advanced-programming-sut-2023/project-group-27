3dadf0ea4f702c137b8b678e645760e34e5e664d
Mobin-Pourabedini <realmobinpourabedini@gmail.com>
2023-04-28 13:40:32 +0430

    complete profile controllers

--- src/main/java/controller/view_controllers/ProfileMenuController.java
+++ src/main/java/controller/view_controllers/ProfileMenuController.java
@@ -2,24 +2,41 @@ package controller.view_controllers;
 
 import controller.controller.CoreProfileMenuController;
 import model.User;
+import view.ProfileMenu;
 
+import java.util.Scanner;
 import java.util.regex.Matcher;
+import controller.controller.Utilities;
+import java.util.regex.Pattern;
 
 public class ProfileMenuController {
     private final CoreProfileMenuController coreController;
-
+    private final ProfileMenu menu;
     private User loggedInUser;
 
-    public ProfileMenuController(CoreProfileMenuController coreController, User loggedInUser) {
+    public ProfileMenuController(CoreProfileMenuController coreController, User loggedInUser, Scanner scanner) {
         this.coreController = coreController;
+        menu = new ProfileMenu(this, scanner);
         this.loggedInUser = loggedInUser;
     }
 
     public String changeUsername(Matcher matcher) {
-        return null;
+        String newUsername = matcher.group("username");
+        if (newUsername.equals(loggedInUser.getUsername())) {
+            return "Username can't be the same as old username";
+        }
+        if (newUsername.equals("")) {
+            return "empty field";
+        }
+        return coreController.changeUsername(newUsername);
     }
 
     public String changeNickname(Matcher matcher) {
+        String newNickname = matcher.group("nickname");
+        if (newNickname.equals("")) {
+            return "empty field";
+        }
+        coreController.changeNickname(newNickname);
         return null;
     }
 
@@ -28,30 +45,68 @@ public class ProfileMenuController {
     }
 
     public String changePassword(Matcher matcher) {
-        return null;
+        String oldPassword = matcher.group("oldPassword");
+        String newPassword = matcher.group("newPassword");
+        if (loggedInUser.isPasswordCorrect(oldPassword)) {
+            return "Current password is incorrect!\n";
+        }
+        if (oldPassword.equals(newPassword)) {
+            return "New password can't be the same as old password";
+        }
+        if (Utilities.validatePassword(newPassword) != null) {
+            return Utilities.validatePassword(newPassword);
+        }
+        if (!menu.confirm("Please enter your new password again", newPassword)) {
+            return "Passwords don't match";
+        }
+        coreController.changePassword(newPassword);
+        return "Successful";
     }
 
     public String changeSlogan(Matcher matcher) {
-        return null;
+        String slogan = matcher.group("slogan");
+        if (slogan.equals("")) {
+            return "empty field";
+        }
+        coreController.changeSlogan(slogan);
+        return "Successful";
+    }
+
+    public String changeEmail(Matcher matcher) {
+        String newEmail = matcher.group("email");
+        if (newEmail.equals("")) {
+            return "empty field";
+        }
+        if (Utilities.validateEmail(newEmail) != null) {
+            return Utilities.validateEmail(newEmail);
+        }
+        coreController.changeEmail(newEmail);
+        return "Successful";
     }
 
     public void removeSlogan() {
-
+        coreController.removeSlogan();
+        // TODO register
     }
 
-    public int showHighScore() {
-        return 1;
+    public String showHighScore() {
+        return String.valueOf(coreController.showHighScore());
     }
 
-    public int showRank() {
-        return 1;
+    public String showRank() {
+        return String.valueOf(coreController.showRank());
     }
 
     public String showSlogan() {
-        return null;
+        return coreController.showSlogan();
     }
 
     public String showProfile() {
-        return null;
+        return coreController.showProfile();
     }
+
+    public ProfileMenu getMenu() {
+        return menu;
+    }
+
 }
