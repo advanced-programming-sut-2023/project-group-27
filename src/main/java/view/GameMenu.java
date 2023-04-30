6742916431bc73ba79665e778f00f270269d41c9
Mobin-Pourabedini <realmobinpourabedini@gmail.com>
2023-04-30 14:08:26 +0430

    Initialize GameMenu

--- src/main/java/view/GameMenu.java
+++ src/main/java/view/GameMenu.java
@@ -1,6 +1,7 @@
 package view;
 
 import controller.view_controllers.GameMenuController;
+import view.enums.GameMenuRegexes;
 
 import java.util.Scanner;
 
@@ -13,6 +14,64 @@ public class GameMenu {
 
     public String run(Scanner scanner) {
         System.out.println("Entered Game Menu");
+        while (true) {
+            String command = scanner.nextLine();
+            String output;
+            if (GameMenuRegexes.SHOW_POPULARITY_FACTORS.getMatcher(command).matches()) {
+                System.out.print(controller.showPopularityFactors());
+                continue;
+            }
+            if (GameMenuRegexes.SHOW_POPULARITY.getMatcher(command).matches()) {
+                System.out.print(controller.showPopularity());
+                continue;
+            }
+            if (GameMenuRegexes.SHOW_FOOD_LIST.getMatcher(command).matches()) {
+                System.out.print(controller.showFoodList());
+                continue;
+            }
+            if (GameMenuRegexes.SHOW_FOOD_RATE.getMatcher(command).matches()) {
+                System.out.print(controller.showFearRate());
+                continue;
+            }
+            if (GameMenuRegexes.SHOW_FEAR_RATE.getMatcher(command).matches()) {
+                System.out.print(controller.showFearRate());
+                continue;
+            }
+            if (GameMenuRegexes.SHOW_TAX_RATE.getMatcher(command).matches()) {
+                System.out.print(controller.showTaxRate());
+                continue;
+            }
+            if (GameMenuRegexes.SET_FEAR_RATE.getMatcher(command).matches()) {
+                output = controller.setFearRate(
+                        GameMenuRegexes.SET_FEAR_RATE.getMatcher(command));
+                System.out.print(output);
+                continue;
+            }
+            if (GameMenuRegexes.SET_FOOD_RATE.getMatcher(command).matches()) {
+                output = controller.setFoodRate(
+                        GameMenuRegexes.SET_FOOD_RATE.getMatcher(command));
+                System.out.print(output);
+                continue;
+            }
+            if (GameMenuRegexes.SET_TAX_RATE.getMatcher(command).matches()) {
+                output = controller.setTaxRate(
+                        GameMenuRegexes.SET_TAX_RATE.getMatcher(command));
+                System.out.print(output);
+                continue;
+            }
+            if (GameMenuRegexes.SELECT_BUILDING.getMatcher(command).matches()) {
+
+            }
+            if (GameMenuRegexes.SELECT_UNIT.getMatcher(command).matches()) {
+
+            }
+            if (GameMenuRegexes.SHOW_MAP.getMatcher(command).matches()) {
+
+            }
+            if (command.equals("Exit")) {
+                break;
+            }
+        }
         return null;
     }
 }
