24258a3b9817bcfbb8016e602d71c664924eb5df
Mazdak <mazdak.tey@gmail.com>
2023-05-13 21:41:36 +0330

    added search task

--- src/main/java/controller/controller/BFS.java
+++ src/main/java/controller/controller/BFS.java
@@ -4,6 +4,8 @@ import model.GameMap;
 import model.Location;
 import model.Movable;
 import model.StrongholdCrusader;
+import model.man.Man;
+import model.man.Soldier;
 
 import java.util.*;
 
@@ -11,6 +13,8 @@ public class BFS {
     private Location startingLocation;
     private GameMap map;
     private final Map<Location, Location> parent = new HashMap<>();
+    private final Map<Location , Integer> height = new HashMap<>();
+    private Soldier target;
 
     public BFS(GameMap map, Movable movable) {
         this.map = map;
@@ -30,6 +34,38 @@ public class BFS {
         }
     }
 
+    public BFS(GameMap map, Movable movable , int maxDepth) {
+        this.target = null;
+        this.map = map;
+        Queue<Location> cells = new LinkedList<>();
+        cells.add(movable.getLocation());
+        startingLocation = cells.peek();
+        height.put(startingLocation , 0);
+        parent.put(startingLocation, null);
+        while (!cells.isEmpty()) {
+            Location currentLocation = cells.poll();
+            List<Man> enemies = map.getCell(currentLocation).getMen().stream().filter(
+                    x -> (x.getOwner() != movable.getOwner() && x instanceof Soldier)).toList();
+            if (enemies.size() > 0) {
+                Random random = new Random();
+                if (target == null) {
+                    this.target = (Soldier) enemies.get(random.nextInt() % enemies.size());
+                }
+            }
+            if (height.get(currentLocation) == maxDepth) {
+                continue;
+            }
+            List<Location> locationNeighbors = currentLocation.getNeighbors(map, movable);
+            for (Location neighbor : locationNeighbors) {
+                if (!parent.containsKey(neighbor)) {
+                    parent.put(neighbor, currentLocation);
+                    cells.add(neighbor);
+                    height.put(neighbor , height.get(currentLocation) + 1);
+                }
+            }
+        }
+    }
+
     public LinkedList<Location> pathTo(Location destination) {
         destination = map.getCell(destination).getLocation();
         if (parent.get(destination) == null) {
@@ -42,5 +78,9 @@ public class BFS {
         }
         Collections.reverse(result);
         return result;
+    }
+
+    public Soldier getTarget() {
+        return target;
     }
 }
---
+++ src/main/java/model/task/Search.java
@@ -0,0 +1,52 @@
+package model.task;
+
+import controller.controller.BFS;
+import model.GameMap;
+import model.Match;
+import model.man.Soldier;
+
+public class Search extends Task{
+    private final Soldier soldier;
+    private BFS bfs;
+    private final GameMap map;
+    private final Match match;
+    private boolean isValid = true;
+
+    public Search(Soldier soldier, GameMap map, Match match) {
+        this.soldier = soldier;
+        this.map = map;
+        this.match = match;
+    }
+
+    @Override
+    public void run() {
+        String state = soldier.getState();
+        int range = 0;
+        switch (state) {
+            case "defensive":
+                range = 5;
+                break;
+            case "standing":
+                range = 0;
+                break;
+            case "offensive":
+                range = 10;
+                break;
+        }
+        bfs = new BFS(map , soldier , range);
+        Soldier target = bfs.getTarget();
+        if (target == null) {
+            isValid = false;
+            return;
+        }
+        Fight fight = new Fight(map , soldier , target);
+        match.addTask(fight);
+        soldier.setTask(fight);
+    }
+
+    @Override
+    public boolean isValid() {
+        if (soldier.getHitPoint() <= 0) return false;
+        return isValid;
+    }
+}
