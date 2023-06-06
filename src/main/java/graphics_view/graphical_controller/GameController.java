69a41fb2438c8fd2b106490e786621c6a1146a83
Arshia <arshiayousefnia@yahoo.com>
2023-06-06 12:32:31 +0330

    implementing game map.

--- src/main/java/graphics_view/graphical_controller/GameController.java
+++ src/main/java/graphics_view/graphical_controller/GameController.java
@@ -8,10 +8,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.TilePane;
+import javafx.scene.layout.*;
+import javafx.scene.shape.Rectangle;
+import model.Cell;
+import model.GameMap;
 import model.Match;
 
 public class GameController {
@@ -28,13 +28,60 @@ public class GameController {
 
     private Match match;
     private CoreGameMenuController controller;
+    private double tileSize = 30.0;
+    GameMap mapData = match.getCurrentMatchMap();
 
     public void init(Match match) {
         this.match = match;
         this.controller = new CoreGameMenuController(match, null);
-
+        initiateGameMap();
     }
 
+    private void initiateGameMap() {
+        gameMap.setVgap(0);
+        gameMap.setHgap(0);
+        gameMap.setPrefRows(mapData.getHeight());
+        gameMap.setPrefColumns(mapData.getWidth());
+        gameMap.setPrefTileHeight(tileSize);
+        gameMap.setPrefTileWidth(tileSize);
+        mountTiles(mapData);
+    }
+
+    private void mountTiles(GameMap selectedMap) {
+        for (int i = selectedMap.getHeight() - 1; i >= 0 ;i--) {
+            for (int j = 0; j < selectedMap.getWidth(); j++) {
+                StackPane tile = getTile(selectedMap.getCell(j, i));
+                gameMap.getChildren().add(tile);
+            }
+        }
+    }
+
+    private StackPane getTile(Cell cell) {
+        StackPane tile = new StackPane();
+        tile.setMinWidth(tileSize);
+        tile.setMaxWidth(tileSize);
+        tile.setMinHeight(tileSize);
+        tile.setMaxHeight(tileSize);
+        mountCellData(tile, cell);
+        return tile;
+    }
+
+    public void mountCellData(StackPane tile, Cell cell) {
+        //TODO add getPicture() method to model entities.
+//        tile.getChildren().add(cell.getType().getPicture());
+//        tile.getChildren().add(cell.getNaturalEntityType().getPicture());
+//        tile.getChildren().add(cell.getBuilding().getPicture());
+//        tile.getChildren().add(cell.getMen().get(0).getPicture());
+        defineClickEvents(tile);
+    }
+
+    private void defineClickEvents(StackPane tile) {
+        //TODO define events such as select, etc...
+    }
+
+    private StackPane getTileByLocation(int x, int y) {
+        return mapData.getCell(x, y).getStackPane();
+    }
 
     public void enterTrade(MouseEvent mouseEvent) throws Exception {
         new TradeMenu(match.getCurrentUser(), match).start(Utilities.getStage());
