1e1573fce7d859837e1e1af3acfefc86ce28bf7f
Arshia <arshiayousefnia@yahoo.com>
2023-06-28 23:12:31 +0330

    initiate lobby class

--- src/main/java/server/GameServer.java
+++ src/main/java/server/GameServer.java
@@ -1,33 +1,25 @@
 package server;
 
 import java.io.IOException;
-import java.net.ServerSocket;
 import java.util.List;
 
 public class GameServer extends Thread{
     int capacity;
     boolean isPublic;
-    ServerSocket server;
     List<Connection> connectionList;
     long creationTime;
 
-    public GameServer(int port, int capacity, int mapIndex, boolean isPublic) throws IOException {
+    public GameServer(int capacity, int mapIndex, boolean isPublic) throws IOException {
         this.creationTime = System.currentTimeMillis() / 1000;
         this.isPublic = isPublic;
         this.capacity = capacity;
-        this.server = new ServerSocket(port);
     }
 
-    @Override
-    public void run() {
-        while (connectionList.size() < capacity) {
-            try {
-                Connection connection = new Connection(server.accept());
-                connectionList.add(connection);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
+    public void addConnection(Connection connection) {
+        connectionList.add(connection);
+    }
 
+    public void removeConnection(Connection connection) {
+        connectionList.remove(connection);
     }
 }
--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -8,8 +8,10 @@ import java.util.List;
 public class Server {
     ServerSocket server;
     List<Connection> connectionList;
+    Lobby lobby;
 
     public Server(int port) throws IOException {
+        this.lobby = new Lobby();
         this.server = new ServerSocket(port);
         while(true) {
             Socket incomingSocket = server.accept();
