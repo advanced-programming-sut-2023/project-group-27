613d471e1782fcf122c59319d8d911ca1ccb53ef
Arshia <arshiayousefnia@yahoo.com>
2023-06-29 10:12:42 +0330

    initiate lobby class

---
+++ src/main/java/graphics_view/graphical_controller/LobbyController.java
@@ -0,0 +1,16 @@
+package graphics_view.graphical_controller;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class LobbyController {
+    public Button createServer;
+    public TextField searchTextField;
+
+    public void createServer(MouseEvent mouseEvent) {
+    }
+
+    public void search(MouseEvent mouseEvent) {
+    }
+}
